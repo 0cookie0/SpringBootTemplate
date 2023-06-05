3cb876c9c93854e625d51789707848498f2cdc97
SuperCookie <2677330142@qq.com>
2023-06-05 21:26:07 +0800

    springboot模版

---
+++ src/main/java/com/javaee/SpringsoottemplateApplication.java
@@ -0,0 +1,13 @@
+package com.javaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsoottemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsoottemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaee/SpringsoottemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsoottemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
