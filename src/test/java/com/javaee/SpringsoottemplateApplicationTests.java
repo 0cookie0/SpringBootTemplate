572db63d71e061f084ad341577ed6bc9ec8dbd55
SuperCookie <2677330142@qq.com>
2023-06-06 17:08:24 +0800

    springboot+mybatis-plus CRUD的测试类

--- src/test/java/com/javaee/SpringsoottemplateApplicationTests.java
+++ src/test/java/com/javaee/SpringsoottemplateApplicationTests.java
@@ -1,5 +1,6 @@
-package com.javaee.test;
+package com.javaee;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.javaee.mapper.UserMapper;
 import com.javaee.po.User;
 import jdk.nashorn.internal.runtime.logging.Logger;
@@ -47,6 +48,23 @@ class SpringsoottemplateApplicationTests {
 	void testGetAll(){
 		List<User> users = userMapper.selectList(null);
 		System.out.println(users);
+	}
+
+	@Test
+	void testGetPage(){
+		Page page = new Page(1,5);
+		/*此功能需要依赖MPConfig,需要注进去一个拦截器的bean*/
+		Page page1 = userMapper.selectPage(page, null);
+		System.out.println(page1);
+		/*当前页*/
+		System.out.println(page1.getCurrent());
+		/*一共几页*/
+		System.out.println(page1.getPages());
+		/*数据*/
+		System.out.println(page1.getRecords());
+		System.out.println(page1.getSize());
+		System.out.println(page1.getTotal());
+
 	}
 
 }
